5291e69d1d9562f91929305cc75cb1d15cba4a5f
Cameron Joy <cameronjoy77@live.com>
2018-08-06 16:29:31 -0700

    added pynput

--- smb.py
+++ smb.py
@@ -5,13 +5,25 @@ import cv2
 import pyscreenshot as pss
 import time
 import sys, string, os
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
 
 
 last_time = time.time()
 while(True):
-    screen = pss.grab(bbox=(50,50,800,640))
+    screen = pss.grab(bbox=(75,75,700,570))
     print('Loop took {} seconds',format(time.time()-last_time))
-    cv2.imshow("test", np.array(screen))
+    edges = []
+
+    edges = cv2.Canny(np.array(screen), 100, 200)
+
+    cv2.imshow("test", np.array(edges))
+
+    keyboard.press(Key.right)
+    time.sleep(3)
+    keyboard.release(Key.right)
+
     last_time = time.time()
     # waits 25 ms for the user to press q and
     # exits if q is pressed
